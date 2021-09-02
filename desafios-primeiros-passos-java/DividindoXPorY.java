3970984afa0e91d34ea50fe3db4f4607bfcfba90
Daniele Araújo <37555137+felurye@users.noreply.github.com>
2021-09-02 19:02:47 -0300

    Adiciona desafio dividindo X por Y

---
+++ desafios-primeiros-passos-java/DividindoXPorY.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Você terá o desafio de escrever um algoritmo que leia 2 números e imprima o resultado da divisão do primeiro pelo segundo.
+ * Caso não for possível, mostre a mensagem “divisao impossivel” para os valores em questão. <br><br>
+ *
+ * Entrada: <br>
+ * A entrada contém um número inteiro N. Este N será a quantidade de pares de valores inteiros (X e Y) que serão lidos em seguida. <br><br>
+ * 
+ * Saída: <br>
+ * Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” caso não seja possível efetuar o cálculo.
+ */
+public class Problem {
+	public static void main(String[] args) throws IOException {
+		Scanner leitor = new Scanner(System.in);
+		int N = leitor.nextInt();
+		int X, Y;
+
+		for (int i = 0; i < N; i++) {
+			X = leitor.nextInt();
+			Y = leitor.nextInt();
+			if (Y == 0)
+				System.out.println("divisao impossivel");
+			else
+				System.out.printf("%.1f\n", ((double) X / Y));
+		}
+	}
+}
