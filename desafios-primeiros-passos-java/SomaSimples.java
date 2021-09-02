381df4aefe20e88f464e897cd10aa67012397e9d
Daniele Araújo <37555137+felurye@users.noreply.github.com>
2021-09-02 19:05:16 -0300

    Formata arquivo

--- desafios-primeiros-passos-java/SomaSimples.java
+++ desafios-primeiros-passos-java/SomaSimples.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 /**
-* Leia dois valores inteiros identificados como variáveis A e B. Calcule a soma entre elas e chame essa variável de SOMA.
-* A seguir escreva o valor desta variável. <br><br>
-*
-* Entrada: <br>
-* O arquivo de entrada contém 2 valores inteiros. <br><br>
-*
-* Saída: <br>
-* Imprima a variável SOMA com todas as letras maiúsculas, inserindo um espaço em branco antes e depois do símbolo de igualdade, seguido pelo valor correspondente à soma de A e B.
-*/
+ * Leia dois valores inteiros identificados como variáveis A e B. Calcule a soma entre elas e chame essa variável de SOMA.
+ * A seguir escreva o valor desta variável. <br><br>
+ *
+ * Entrada: <br>
+ * O arquivo de entrada contém 2 valores inteiros. <br><br>
+ *
+ * Saída: <br>
+ * Imprima a variável SOMA com todas as letras maiúsculas, inserindo um espaço em branco antes e depois do símbolo de igualdade, seguido pelo valor correspondente à soma de A e B.
+ */
 public class SomaSimples {
   public static void main(String[] args) {
     Scanner sc = new Scanner (System.in);
